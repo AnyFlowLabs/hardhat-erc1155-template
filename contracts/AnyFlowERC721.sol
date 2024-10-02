36c1c0e4896176c9e723ed19f9e05b51bb07c4ad
Yudi Kubota <yudikubota@gmail.com>
2024-10-02 16:33:12 -0300

    Change to ERC1155

--- contracts/AnyFlowERC721.sol
+++ contracts/AnyFlowERC721.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AnyFlowERC721 is ERC721, Ownable {
+contract AnyFlowERC1155 is ERC1155, Ownable {
     constructor(
         address _initialOwner,
-        string memory _name,
-        string memory _symbol
-    ) ERC721(_name, _symbol) Ownable(_initialOwner) {}
+        string memory uri_
+    ) ERC1155(uri_) Ownable(_initialOwner) {}
 
     // Add additional logic here
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,16 @@
 import hre from "hardhat";
 
 export async function main() {
-    console.log('Deploying AnyFlowERC721...')
+    console.log('Deploying AnyFlowERC1155...')
 
     const args = [
         '0xA71a71A71a71a71A71a71a71a71A71a71A71A71a',   // _initialOwner
-        'AnyFlowERC721', // _name
-        'AF',           // _symbol
+        'https://token-cdn-domain/{id}.json'
     ] as const;
 
-    const { deploymentTransaction } = await hre.viem.sendDeploymentTransaction("AnyFlowERC721", args);
+    const { deploymentTransaction } = await hre.viem.sendDeploymentTransaction("AnyFlowERC1155", args);
 
-    console.log(`Deployment transaction for AnyFlowERC721 broadcasted... tx: ${deploymentTransaction.hash}`);
+    console.log(`Deployment transaction for AnyFlowERC1155 broadcasted... tx: ${deploymentTransaction.hash}`);
 
     // With AnyFlow you do not need to wait for the transaction to be mined
 }
