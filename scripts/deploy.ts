cbf362955791500c5255e7eb4b57bb11ead26f3d
Yudi Kubota <yudikubota@gmail.com>
2024-10-30 16:09:07 -0300

    change hardhat-viem deployment method

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,11 +8,10 @@ export async function main() {
         'https://token-cdn-domain/{id}.json'
     ] as const;
 
-    const { deploymentTransaction } = await hre.viem.sendDeploymentTransaction("AnyFlowERC1155", args);
+    const contract = await hre.viem.deployContract("AnyFlowERC1155", args);
 
-    console.log(`Deployment transaction for AnyFlowERC1155 broadcasted... tx: ${deploymentTransaction.hash}`);
-
-    // With AnyFlow you do not need to wait for the transaction to be mined
+    console.log(`Contract AnyFlowERC721 deployed to: ${contract.address}`);
 }
 
 main()
+    .then(() => process.exit(0));
